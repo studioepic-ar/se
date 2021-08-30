b0eb9137ba548607539b6baa94cf48d196519a50
Preetham M.S <preethamms619@gmail.com>
2021-08-30 20:25:49 +0530

    add ProjectsPage

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import SlideShow from "./components/Carousel/Carousel.component";
 import Homepage from "./pages/Homepage/Homepage.component";
+import ProjectPage from "./pages/ProjectsPage/Project.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/projects" component={ProjectPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/ProjectsPage/Project.component.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+import NavBar from "../../components/NavBar/Navbar.component";
+
+const ProjectContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 50%;
+  height: 300px;
+  border: solid 1px;
+  background-color: red;
+  overflow: hidden;
+
+  @media only screen and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+`;
+
+const ProjectImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url("https://source.unsplash.com/random");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    overflow: hidden;
+  }
+`;
+
+const ProjectTitle = styled.h1`
+  position: absolute;
+  bottom: 1.5rem;
+  left: 2.5rem;
+  letter-spacing: 0.5px;
+  color: white;
+  font-weight: 500;
+  font-size: 1.5rem;
+`;
+
+export default function ProjectPage() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <p
+          className="text-4xl text-center m-8 tracking-wider"
+          data-aos="fade-down"
+        >
+          PROJECTS
+        </p>
+      </div>
+      <div className="flex flex-wrap">
+        <ProjectContainer data-aos="fade-right">
+          <ProjectImage />
+          <ProjectTitle data-aos="">Text here</ProjectTitle>
+        </ProjectContainer>
+        <ProjectContainer data-aos="fade-left">
+          <ProjectImage />
+          <ProjectTitle>Text here</ProjectTitle>
+        </ProjectContainer>
+        <ProjectContainer data-aos="fade-right">
+          <ProjectImage />
+          <ProjectTitle>Text here</ProjectTitle>
+        </ProjectContainer>
+        <ProjectContainer data-aos="fade-left">
+          <ProjectImage />
+          <ProjectTitle>Text here</ProjectTitle>
+        </ProjectContainer>
+        <ProjectContainer data-aos="fade-right">
+          <ProjectImage />
+          <ProjectTitle>Text here</ProjectTitle>
+        </ProjectContainer>
+        <ProjectContainer data-aos="fade-left">
+          <ProjectImage />
+          <ProjectTitle>Text here</ProjectTitle>
+        </ProjectContainer>
+      </div>
+    </div>
+  );
+}
