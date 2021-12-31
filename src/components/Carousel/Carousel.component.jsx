abaf2bf130a41f659d0571b4a8c7615571aceb17
Preetham M.S <preethamms619@gmail.com>
2022-01-01 01:28:44 +0530

    add CarouselData and change in Homepage

--- src/components/Carousel/Carousel.component.jsx
+++ src/components/Carousel/Carousel.component.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import styled from "styled-components";
+import { CarouselData } from "./CarouselData";
 
 const CImage = styled.div`
   background-position: center center;
@@ -28,11 +29,20 @@ const CMessage = styled.div`
   margin-bottom: 3rem;
   /* margin: 2rem 3rem; */
   letter-spacing: 0.5px;
+  /* background-color: #00000021; */
+  font-weight: 600;
+
+  @media only screen and (min-width: 1024px) {
+    width: 55%;
+  }
 `;
 
 function SlideShow() {
+  const [carouselItems, setCarouselItems] = useState({ CarouselData });
+  const items = carouselItems.CarouselData.items;
+
   return (
-    <div className=" top-0 left-0">
+    <div className="top-0 left-0">
       <Carousel
         autoPlay
         infiniteLoop
@@ -42,38 +52,18 @@ function SlideShow() {
         showThumbs={false}
         interval={3000}
       >
-        <CImage image="https://i.ibb.co/kmVsQ1s/r-architecture-2g-Dwl-Iim3-Uw-unsplash.jpg">
-        {/* <CImage image="https://i.ibb.co/xXGVsT0/marc-olivier-jodoin-o-Thn-HPkb3p8-unsplash.jpg"> */}
-        {/* <CImage image="https://i.ibb.co/k9JRDdJ/li-yang-Co-Wc-Jmb-UCWw-unsplash.jpg"> */}
-          <CMessage>
-            <h1 className="text-2xl sm:text-3xl font-light italic lg:w-3/5">
-              “One of the great beauties of architecture is that each time, it
-              is like life starting all over again.”
-            </h1>
-            <p className="text-lg sm:text-xl">– Renzo Piano</p>
-          </CMessage>
-        </CImage>
-
-        <CImage image="https://i.ibb.co/k9JRDdJ/li-yang-Co-Wc-Jmb-UCWw-unsplash.jpg">
-          <CMessage>
-            <h1 className="text-2xl sm:text-3xl font-light italic lg:w-3/5">
-              “The mother art is architecture. Without an architecture of our
-              own we have no soul of our own civilization.”
-            </h1>
-            <p className="text-lg sm:text-xl">- Frank Lloyd Wright</p>
-          </CMessage>
-        </CImage>
-
-        <CImage image="https://i.ibb.co/xSxZbHc/kir-simakov-1y5-Pk-TZOMb-Q-unsplash.jpg">
-          <CMessage>
-            <h1 className="text-2xl sm:text-3xl font-light italic lg:w-3/5">
-            “Architecture should speak of its time and place, but yearn for timelessness.”  
-            </h1>
-            <p className="text-lg sm:text-xl">
-            – Frank Gehry
-            </p>
-          </CMessage>
-        </CImage>
+        {items.map(({ id, imageUrl, heading, subHeading }) => (
+          <CImage image={imageUrl} id={id}>
+            <CMessage>
+              <h1 className="text-2xl font-medium tracking-wide text-black">
+                {heading}
+              </h1>
+              <p className="text-lg italic font-normal tracking-wide text-black">
+                {subHeading}
+              </p>
+            </CMessage>
+          </CImage>
+        ))}
       </Carousel>
     </div>
   );
---
+++ src/components/Carousel/CarouselData.js
@@ -0,0 +1,28 @@
+export const CarouselData = {
+  id: 1,
+  title: "Carousel",
+  routeName: "/",
+  items: [
+    {
+      id: 3,
+      imageUrl: "https://i.ibb.co/R9vVqTL/homepage4.jpg",
+      heading:
+        '"Architecture should speak of its time and place, but yearn for timelessness."',
+      subHeading: "– Frank Gehry",
+    },
+    {
+      id: 1,
+      imageUrl: "https://i.ibb.co/tzk6tbr/homepage2.jpg",
+      heading:
+        '"One of the great beauties of architecture is that each time, it is like life starting all over again."',
+      subHeading: "– Renzo Piano",
+    },
+    {
+      id: 2,
+      imageUrl: "https://i.ibb.co/xjpTqhx/homepage3.jpg",
+      heading:
+        '"The mother art is architecture. Without an architecture of our own we have no soul of our own civilization."',
+      subHeading: "– Frank Lloyd Wright",
+    },
+  ],
+};
