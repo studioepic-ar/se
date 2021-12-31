d18b3cab6b76ed473c79114317f1fbe774ec3c1a
Preetham M.S <preethamms619@gmail.com>
2021-12-31 21:29:22 +0530

    add content to project page

--- src/pages/ProjectsPage/Project.component.jsx
+++ src/pages/ProjectsPage/Project.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import Aos from "aos";
@@ -6,16 +6,17 @@ import "aos/dist/aos.css";
 
 import NavBar from "../../components/NavBar/Navbar.component";
 
+import { Project } from "./ProjectData";
+
 const ProjectContainer = styled.div`
   position: relative;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   width: 50%;
-  height: 300px;
+  height: 350px;
   border: solid 1px;
   background-color: transparent;
-  /* background-color: red; */
   overflow: hidden;
 
   @media only screen and (max-width: 767px) {
@@ -28,77 +29,71 @@ const ProjectContainer = styled.div`
 const ProjectImage = styled.div`
   width: 100%;
   height: 100%;
-  background-image: url("https://source.unsplash.com/random");
+  background-image: url(${(props) => props.projectUrl});
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
-  transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+  transition: transform 1500ms cubic-bezier(0.25, 0.45, 0.45, 0.95);
 
   &:hover {
-    transform: scale(1.1);
-    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    transform: scale(1.05);
+    transition: transform 1500ms cubic-bezier(0.25, 0.45, 0.45, 0.95);
     overflow: hidden;
   }
 `;
 
-const ProjectTitle = styled.h1`
+const ProjectTitle = styled.p`
   position: absolute;
   bottom: 1.5rem;
-  left: 2.5rem;
+  /* left: 2.5rem; */
+  width: 100%;
+  margin: auto;
   letter-spacing: 0.5px;
+  background-color: #00000021;
   color: white;
-  font-weight: 500;
-  font-size: 1.5rem;
+  font-weight: 600;
+  padding: 3px;
+  text-align: center;
+
+  @media only screen and (max-width: 767px) {
+    font-size: 13px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+
+  @media only screen and (min-width: 1279px) {
+    font-size: 18px;
+  }
 `;
 
 export default function ProjectPage() {
+  const [projectItems, setProjectItems] = useState({ Project });
+  const items = projectItems.Project.items;
+
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
+
   return (
     <div>
       <NavBar />
       <div>
         <p
-          className="text-4xl text-center m-8 tracking-wider"
+          className="text-3xl sm:text-4xl text-center m-8 tracking-wider"
           data-aos="fade-down"
         >
           PROJECTS
         </p>
       </div>
       <div className="flex flex-wrap">
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle data-aos="">Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle>Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle data-aos="">Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle>Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle data-aos="">Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle>Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle data-aos="">Text here</ProjectTitle>
-        </ProjectContainer>
-        <ProjectContainer data-aos="zoom-in">
-          <ProjectImage />
-          <ProjectTitle>Text here</ProjectTitle>
-        </ProjectContainer>
+        {items.map(({ id, projectTitle, projectImage }) => (
+          <ProjectContainer data-aos="zoom-in">
+            <ProjectImage projectUrl={projectImage} />
+            <ProjectTitle>{projectTitle}</ProjectTitle>
+          </ProjectContainer>
+        ))}
       </div>
     </div>
   );
---
+++ src/pages/ProjectsPage/ProjectData.js
@@ -0,0 +1,59 @@
+export const Project = {
+  id: 1,
+  title: "Projects",
+  routeName: "project",
+  items: [
+    {
+      id: 1,
+      projectTitle: "HARISH RESIDENCE, SHIVAMOGGA",
+      projectImage: "https://i.ibb.co/PDPyh7S/render04-11zon.jpg",
+    },
+    {
+      id: 2,
+      projectTitle: "L.I.C. PREMIUM COLLECTION POINT, SHIVAMOGGA",
+      projectImage:
+        "https://i.ibb.co/6WsZvRf/OPTION-3-WITHOUT-HUMANS02-11zon.jpg",
+    },
+    {
+      id: 3,
+      projectTitle: "RAITA BHAVANA FOR KVP CONSTRUCTIONS, SHIKHARIPURA",
+      projectImage: "https://i.ibb.co/TR4KJ66/VIEWS12-11zon.jpg",
+    },
+    {
+      id: 4,
+      projectTitle: "THE PARK VIEW RESIDENCE, SHIVAMOGGA",
+      projectImage: "https://i.ibb.co/16q5Gyj/aa.jpg",
+    },
+    {
+      id: 5,
+      projectTitle: "A.K.V. RESIDENCE RENOVATION, SHIVAMOGGA",
+      projectImage: "https://i.ibb.co/Xx95MJh/RENDERS10-11zon.jpg",
+    },
+    {
+      id: 6,
+      projectTitle: "SUDA OFFICE INTERIORS FOR KVP CONSTRUCTIONS",
+      projectImage: "https://i.ibb.co/NjgVkTb/5-FRONT-OFFICE.jpg",
+    },
+    {
+      id: 7,
+      projectTitle: "TWO-WHEELER AUTO WORLD COMPLEX, SHIVAMOGGA",
+      projectImage: "https://i.ibb.co/jkcXV28/renders-1st02-11zon.jpg",
+    },
+    {
+      id: 8,
+      projectTitle: "SRINATH RESIDENCE, SHIVAMOGGA",
+      //todo: change image here
+      projectImage: "https://source.unsplash.com/random",
+    },
+    {
+      id: 9,
+      projectTitle: "SARATHI FARM HOUSE, SRINGERI",
+      projectImage: "https://i.ibb.co/80rvZLW/day02-11zon.jpg",
+    },
+    {
+      id: 10,
+      projectTitle: "HOSAMANE LAYOUT MASTER PLANNING, SRINGERI",
+      projectImage: "https://i.ibb.co/bKqBnX8/hosmane-layout-A03-11zon.jpg",
+    },
+  ],
+};
