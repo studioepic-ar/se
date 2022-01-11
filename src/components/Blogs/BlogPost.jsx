f651f91e481d8008248d42299622552c682fba41
Preetham M.S <preethamms619@gmail.com>
2022-01-11 17:06:56 +0530

    add allBlogs and singleBlog

--- src/components/Blogs/BlogPost.jsx
+++ src/components/Blogs/BlogPost.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import sanityClient from "../../client.js";
 import { Link } from "react-router-dom";
+import NavBar from "../NavBar/Navbar.component.jsx";
 
 function BlogPost() {
   const [blogData, setBlogData] = useState(null);
@@ -24,41 +25,58 @@ function BlogPost() {
       .catch(console.error);
   }, []);
 
+  if (!blogData)
+    return (
+      <div className="flex items-center justify-center space-x-2 animate-pulse m-auto w-1/2 h-screen">
+        <div
+          style={{ borderTopColor: "transparent" }}
+          className="w-16 h-16 border-4 border-black border-solid
+        rounded-full animate-spin"
+        ></div>
+      </div>
+    );
+
   return (
-    <main className="bg-green-100 min-h-screen p-12">
-      <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive">
-          Blog Posts Page
-        </h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my page of blog posts
-        </h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogData &&
-            blogData.map((post, index) => (
-              <article>
-                <Link to={"/blog/" + post.slug.current} key={post.slug.current}>
-                  <span
-                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
-                    key={index}
+    <div>
+      <NavBar />
+      <main className="px-12">
+        <section className="container mx-auto">
+          <h1 className="text-3xl sm:text-4xl text-center mt-8 tracking-wider">
+            Blog Posts Page
+          </h1>
+          <h2 className="text-xl sm:text-2xl text-center mt-2 mb-8 tracking-wide">
+            Welcome to my page of blog posts
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {blogData &&
+              blogData.map((post, index) => (
+                <article>
+                  <Link
+                    to={"/blog/" + post.slug.current}
+                    key={post.slug.current}
                   >
-                    <img
-                      src={post.mainImage.asset.url}
-                      alt={post.mainImage.alt}
-                      className="w-full h-full rounded-r object-cover absolute"
-                    />
-                    <span className="relative h-full flex justify-end items-end pr-4 pb-4">
-                      <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-100 bg-opacity-75 rounded">
-                        {post.title}
-                      </h3>
+                    <span
+                      className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-black border "
+                      key={index}
+                    >
+                      <img
+                        src={post.mainImage.asset.url}
+                        alt={post.mainImage.alt}
+                        className="w-full h-full rounded-r object-cover absolute"
+                      />
+                      <span className="relative h-full flex justify-end items-end pr-4 pb-4">
+                        <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-white-100 bg-opacity-75 rounded">
+                          {post.title}
+                        </h3>
+                      </span>
                     </span>
-                  </span>
-                </Link>
-              </article>
-            ))}
-        </div>
-      </section>
-    </main>
+                  </Link>
+                </article>
+              ))}
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
 
--- src/components/Blogs/SinglePost.jsx
+++ src/components/Blogs/SinglePost.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+import NavBar from "../NavBar/Navbar.component.jsx";
 
-function SinglePost() {
-    return (
-        <div>
-            <h1>single post</h1>
-        </div>
-    )
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
 }
 
-export default SinglePost
+function SinglePost() {
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{
+            title,
+            _id,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+            },
+            body,
+            "name": author->name,
+            "authorImage": author->image
+        }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!singlePost)
+    return (
+      <div className="flex items-center justify-center space-x-2 animate-pulse m-auto w-1/2 h-screen">
+        <div
+          style={{ borderTopColor: "transparent" }}
+          className="w-16 h-16 border-4 border-black border-solid
+        rounded-full animate-spin"
+        ></div>
+      </div>
+    );
+
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <article className="container shadow-lg mx-auto bg-gray-100 rounded-lg">
+          <header className="relative">
+            <div className="absolute h-full w-full flex items-center justify-center p-8">
+              {/* title and author block */}
+              <div className="bg-white bg-opacity-75 rounded flex flex-col m-auto items-center text-center z-50 p-4 sm:p-5 md:p-6 lg:p-8">
+                <h1 className="text-3xl sm:text-4xl text-center mt-8 tracking-wider">
+                  {singlePost.title}
+                </h1>
+                {/* image and author div */}
+                <div className="flex items-start justify-center text-gray-800 mt-1">
+                  <img
+                    src={urlFor(singlePost.authorImage).url()}
+                    alt={singlePost.name}
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <p className="text-lg sm:text-xl text-center mt-2 mb-8 ml-2 tracking-wide">
+                    {singlePost.name}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img
+              src={singlePost.mainImage.asset.url}
+              alt={singlePost.title}
+              className="w-full object-cover rounded-t"
+              style={{ height: "400px" }}
+            />
+          </header>
+          <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full text-black">
+            <BlockContent
+              blocks={singlePost.body}
+              projectId="xpjb16mq"
+              dataset="production"
+            />
+          </div>
+        </article>
+      </main>
+    </div>
+  );
+}
+
+export default SinglePost;
