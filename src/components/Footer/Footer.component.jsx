051fa59353815c3313b368a6a0dcc9dc9095b264
Preetham M.S <preethamms619@gmail.com>
2022-01-12 23:18:24 +0530

    add footer

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  var date = new Date().getFullYear();
+  return (
+    <div className="m-auto text-center">
+      <h1 className="tracking-wider opacity-80 p-4 text-xs sm:text-sm md:text-base">
+        STUDIO EPIC LLP ® 2021-{date} | All Rights Reserved
+      </h1>
+    </div>
+  );
+}
+
+export default Footer;
