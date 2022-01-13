b08063a7e8bee65edd1cca180ed4590c3661b741
Preetham M.S <preethamms619@gmail.com>
2022-01-13 18:32:20 +0530

    fix footer font size

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -4,7 +4,7 @@ function Footer() {
   var date = new Date().getFullYear();
   return (
     <div className="m-auto text-center">
-      <h1 className="tracking-wider opacity-80 p-4 text-xs sm:text-sm md:text-base">
+      <h1 className="tracking-wider opacity-80 p-4 text-xs sm:text-sm">
         STUDIO EPIC LLP ® 2021-{date} | All Rights Reserved
       </h1>
     </div>
